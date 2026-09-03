a817423bdf2029e1d3b56fac335e6e1dfbad16a3
yarosfactory-twitch <yarosfactory@gmail.com>
2026-09-03 22:20:19 +0300

    common.js

--- common.js
+++ common.js
@@ -91,7 +91,8 @@ function fireConfetti(){
     style.textContent = `
       @keyframes confettiFall{
         0%{ transform:translateY(-20px) rotate(0deg); opacity:1; }
-        100%{ transform:translateY(100vh) rotate(720deg); opacity:0.85; }
+        80%{ opacity:1; }
+        100%{ transform:translateY(100vh) rotate(720deg); opacity:0; }
       }
     `;
     document.head.appendChild(style);
